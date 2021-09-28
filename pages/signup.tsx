b9a9531271ea6fb07cea7ae22ee6477715c6404a
Renato Siqueira <renatoelysiqueira@gmail.com>
2021-09-28 18:49:30 -0300

    signup page

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -5,7 +5,6 @@ import Link from 'next/link'
 import { toast } from 'react-toastify'
 import { useFormik } from 'formik'
 import * as Yup from 'yup'
-import { useRouter } from 'next/router'
 import { useMutation } from 'urql'
 import ReCaptcha from 'react-google-recaptcha'
 
@@ -13,6 +12,7 @@ import { AccountContext } from '../context/AccountContext'
 
 import Head from '../elements/Head'
 import Logo from '../elements/Logo'
+import ConfirmEmail from '../components/ConfirmEmail'
 
 interface FormValues {
   firstName: string
@@ -45,9 +45,9 @@ const validationSchema = Yup.object().shape({
 })
 
 const SignUp = () => {
-  const { push } = useRouter()
+  const [confirmEmail, setConfirmEmail] = useState(false)
   const { id, friendlyName, recaptchaSiteKey } = useContext(AccountContext)
-  const [, auth] = useMutation(CREATE_USER)
+  const [, create] = useMutation(CREATE_USER)
   const captchaRef = useRef()
   const [siteKey, setSiteKey] = useState('')
 
@@ -64,36 +64,24 @@ const SignUp = () => {
   }, [recaptchaSiteKey, production])
 
   const onSubmit = async (values: FormValues) => {
-    const input = { UserInput: { ...values }, accountId: id }
+    const input = {
+      SignUpUserInput: {
+        ...values,
+        recaptcha: production ? captchaRef.current.getValue() : ''
+      },
+      accountId: id
+    }
     try {
-      const { data } = await auth(input)
+      production && (await captchaRef.current.execute())
+      const { data } = await create(input)
       if (!data) {
-        toast.error('Usuário e/ou senha inválidos.')
+        toast.error('Erro ao criar conta.')
         return
       }
-      localStorage.setItem('learn49-token', data.auth.token)
-      localStorage.setItem('learn49-user', JSON.stringify(data.auth.user))
-      push('/app')
+      setConfirmEmail(true)
     } catch (e) {
       toast.error('Erro ao tentar operação')
     }
-    // try {
-    //   production && (await captchaRef.current.execute())
-    //   await signUpUser({
-    //     variables: {
-    //       accountId,
-    //       SignUpUserInput: {
-    //         recaptcha: production ? captchaRef.current.getValue() : '',
-    //         ...data
-    //       }
-    //     }
-    //   })
-    // } catch (error) {
-    //   handleOpenSnackbar({
-    //     severity: 'error',
-    //     message: 'Erro ao criar conta.'
-    //   })
-    // }
   }
 
   const form = useFormik({
@@ -113,134 +101,140 @@ const SignUp = () => {
       <Head title={`${friendlyName} - Login`} />
       <div className={`min-h-screen bg-gray-50 dark:bg-gray-900`}>
         <Logo />
-        <div className='flex items-center pt-10 md:pt-2 lg:pt-14'>
-          <div className='flex-1 h-full max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800'>
+        <div className='flex items-center pt-10 md:pt-0 lg:pt-14'>
+          <div className='flex-1 h-full max-w-4xl mx-auto overflow-hidden bg-white lg:rounded-lg shadow-xl dark:bg-gray-800'>
             <div className='flex flex-col overflow-y-auto md:flex-row'>
               <div className='bg-initial flex bg-cover bg-center w-full md:w-1/2 h-64 md:h-auto'></div>
               <main className='flex itembg-cover bg-centers-center justify-center p-6 sm:px-8 md:w-1/2'>
                 <div className='w-full'>
-                  <h1 className='mb-4 text-xl font-semibold text-gray-700 dark:text-gray-200 capitalize'>
+                  <h1 className='mb-4 text-xl font-semibold text-gray-600 dark:text-gray-200 capitalize'>
                     {friendlyName}
                   </h1>
-                  <p className='text-gray-500 -mt-5 mb-3'>
-                    É bom ter você aqui! Crie uma conta!
-                  </p>
-                  <form onSubmit={form.handleSubmit}>
-                    <Label>
-                      <span>Nome</span>
-                      <Input
-                        css='mt-1 border border-opacity-50'
-                        type='text'
-                        id='firstName'
-                        valid={!form.errors.firstName}
-                        placeholder='Nome'
-                        onChange={form.handleChange}
-                        onBlur={form.handleBlur}
-                      />
-                      {form.errors.firstName && (
-                        <HelperText className='text-red-300'>
-                          {form.errors.firstName}
-                        </HelperText>
-                      )}
-                    </Label>
-                    <Label className='mt-2'>
-                      <span>Último Nome</span>
-                      <Input
-                        css='mt-1 border border-opacity-50 border-gray-200'
-                        type='text'
-                        id='lastName'
-                        valid={!form.errors.lastName}
-                        placeholder='Nome'
-                        onChange={form.handleChange}
-                        onBlur={form.handleBlur}
-                      />
-                      {form.errors.lastName && (
-                        <HelperText className='text-red-300'>
-                          {form.errors.lastName}
-                        </HelperText>
-                      )}
-                    </Label>
-                    <Label className='mt-2'>
-                      <span>Email</span>
-                      <Input
-                        css='mt-1 border border-opacity-50 border-gray-200'
-                        type='email'
-                        id='email'
-                        valid={form.submitCount == 0 || !form.errors.email}
-                        placeholder='Digite seu email'
-                        onChange={form.handleChange}
-                        onBlur={form.handleBlur}
-                      />
-                      {form.errors.email && (
-                        <HelperText className='text-red-300'>
-                          {form.errors.email}
-                        </HelperText>
-                      )}
-                    </Label>
-                    <Label className='mt-2'>
-                      <span>Senha</span>
-                      <Input
-                        css='mt-1 border border-opacity-50 border-gray-200'
-                        type='password'
-                        id='passwd'
-                        valid={!form.errors.passwd}
-                        placeholder='Digite sua senha'
-                        onChange={form.handleChange}
-                        onBlur={form.handleBlur}
-                      />
-                      {form.errors.passwd && (
-                        <HelperText className='text-red-300'>
-                          {form.errors.passwd}
-                        </HelperText>
-                      )}
-                    </Label>
-                    <Label check className='mt-2'>
-                      <Input
-                        css=''
-                        type='checkbox'
-                        name='acceptTerms'
-                        onChange={form.handleChange}
-                        onBlur={form.handleBlur}
-                      />
-                      <span className='ml-2'>
-                        Criar uma conta significa que você concorda com nossos{' '}
-                        <Link href='/accept-terms' passHref>
-                          <a className='text-gray-800 font-medium hover:underline hover:text-gray-600'>
-                            Termos de Serviço,
-                          </a>
-                        </Link>{' '}
-                        <Link href='/policies' passHref>
-                          <a className='text-gray-800 font-medium hover:underline hover:text-gray-600'>
-                            Política de Privacidade
-                          </a>
-                        </Link>{' '}
-                        e nossas{' '}
-                        <Link href='/policies' passHref>
-                          <a className='text-gray-800 font-medium hover:underline hover:text-gray-600'>
-                            Configurações de Notificações.
-                          </a>
-                        </Link>
-                      </span>
-                    </Label>
-                    {form.errors.acceptTerms && (
-                      <HelperText className='text-red-300'>
-                        {form.errors.acceptTerms}
-                      </HelperText>
-                    )}
+                  {confirmEmail && <ConfirmEmail />}
+                  {!confirmEmail && (
+                    <>
+                      <p className='text-gray-500 -mt-5 mb-3'>
+                        É bom ter você aqui! Crie uma conta!
+                      </p>
+                      <form onSubmit={form.handleSubmit}>
+                        <Label>
+                          <span>Nome</span>
+                          <Input
+                            css='mt-1 border border-opacity-50'
+                            type='text'
+                            id='firstName'
+                            valid={!form.errors.firstName}
+                            placeholder='Nome'
+                            onChange={form.handleChange}
+                            onBlur={form.handleBlur}
+                          />
+                          {form.errors.firstName && (
+                            <HelperText className='text-red-300'>
+                              {form.errors.firstName}
+                            </HelperText>
+                          )}
+                        </Label>
+                        <Label className='mt-2'>
+                          <span>Último Nome</span>
+                          <Input
+                            css='mt-1 border border-opacity-50 border-gray-200'
+                            type='text'
+                            id='lastName'
+                            valid={!form.errors.lastName}
+                            placeholder='Nome'
+                            onChange={form.handleChange}
+                            onBlur={form.handleBlur}
+                          />
+                          {form.errors.lastName && (
+                            <HelperText className='text-red-300'>
+                              {form.errors.lastName}
+                            </HelperText>
+                          )}
+                        </Label>
+                        <Label className='mt-2'>
+                          <span>Email</span>
+                          <Input
+                            css='mt-1 border border-opacity-50 border-gray-200'
+                            type='email'
+                            id='email'
+                            valid={form.submitCount == 0 || !form.errors.email}
+                            placeholder='Digite seu email'
+                            onChange={form.handleChange}
+                            onBlur={form.handleBlur}
+                          />
+                          {form.errors.email && (
+                            <HelperText className='text-red-300'>
+                              {form.errors.email}
+                            </HelperText>
+                          )}
+                        </Label>
+                        <Label className='mt-2'>
+                          <span>Senha</span>
+                          <Input
+                            css='mt-1 border border-opacity-50 border-gray-200'
+                            type='password'
+                            id='passwd'
+                            valid={!form.errors.passwd}
+                            placeholder='Digite sua senha'
+                            onChange={form.handleChange}
+                            onBlur={form.handleBlur}
+                          />
+                          {form.errors.passwd && (
+                            <HelperText className='text-red-300'>
+                              {form.errors.passwd}
+                            </HelperText>
+                          )}
+                        </Label>
+                        <Label check className='mt-2'>
+                          <Input
+                            css=''
+                            type='checkbox'
+                            name='acceptTerms'
+                            onChange={form.handleChange}
+                            onBlur={form.handleBlur}
+                          />
+                          <span className='ml-2'>
+                            Criar uma conta significa que você concorda com
+                            nossos{' '}
+                            <Link href='/accept-terms' passHref>
+                              <a className='text-gray-800 font-medium hover:underline hover:text-gray-600'>
+                                Termos de Serviço,
+                              </a>
+                            </Link>{' '}
+                            <Link href='/policies' passHref>
+                              <a className='text-gray-800 font-medium hover:underline hover:text-gray-600'>
+                                Política de Privacidade
+                              </a>
+                            </Link>{' '}
+                            e nossas{' '}
+                            <Link href='/policies' passHref>
+                              <a className='text-gray-800 font-medium hover:underline hover:text-gray-600'>
+                                Configurações de Notificações.
+                              </a>
+                            </Link>
+                          </span>
+                        </Label>
+                        {form.errors.acceptTerms && (
+                          <HelperText className='text-red-300'>
+                            {form.errors.acceptTerms}
+                          </HelperText>
+                        )}
 
-                    <Button
-                      className='mt-4'
-                      block
-                      type={'submit'}
-                      disabled={form.isSubmitting}
-                    >
-                      {form.isSubmitting ? 'Aguarde...' : 'Cadastrar'}
-                    </Button>
-                  </form>
+                        <Button
+                          className='mt-4'
+                          block
+                          type={'submit'}
+                          disabled={form.isSubmitting}
+                        >
+                          {form.isSubmitting ? 'Aguarde...' : 'Cadastrar'}
+                        </Button>
+                      </form>
+                    </>
+                  )}
                   <hr className='my-3' />
                   <div className='flex justify-between'>
                     <Link href='/'>
-                      <a className='text-sm text-gray-500 hover:text-gray-600 hover:underline cursor-pointer'>
+                      <a className='text-sm text-gray-500 hover:text-gray-600 hover:underline cursor-pointer font-semibold'>
                         Fazer Login
                       </a>
                     </Link>
@@ -253,14 +247,14 @@ const SignUp = () => {
                       />
                     </div>
                   </div>
+                  {siteKey && (
+                    <ReCaptcha
+                      ref={captchaRef}
+                      size='invisible'
+                      sitekey={siteKey}
+                    />
+                  )}
                 </div>
-                {siteKey && (
-                  <ReCaptcha
-                    ref={captchaRef}
-                    size='invisible'
-                    sitekey={siteKey}
-                  />
-                )}
               </main>
             </div>
           </div>
