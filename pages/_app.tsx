986f2c9a373d6bda69d81c81936416d1a44ef220
Renato Siqueira <renatoelysiqueira@gmail.com>
2021-09-28 18:49:48 -0300

    added forgot page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,7 @@ import ToastElement from '../elements/Toast'
 import AdminLayout from '../layouts/AdminLayout'
 import ParentLayout from '../layouts/ParentLayout'
 
-const nonAuthenticate = ['/', '/signup', '/404', '/terms', '/admin']
+const nonAuthenticate = ['/', '/signup', '/forgot', '/404', '/terms', '/admin']
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps, router }) => {
   const { account } = pageProps
