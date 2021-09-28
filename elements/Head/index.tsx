ee3163caefc83c4a231f693b2d0933332cac62a9
Renato Siqueira <renatoelysiqueira@gmail.com>
2021-09-28 19:34:42 -0300

    added SEO

--- elements/Head/index.tsx
+++ elements/Head/index.tsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import Head from 'next/head'
+import { NextSeo } from 'next-seo'
 
 interface Props {
   title: string
 }
 
 const CustomHead = ({ title }: Props) => (
-  <Head>
-    <title>{title} - Learn49</title>
-    <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-  </Head>
+  <>
+    <NextSeo title={title} description={title} defaultTitle={title} />
+    <Head>
+      <title>{title} - Learn49</title>
+      <meta charSet='utf-8' />
+      <meta httpEquiv='Content-Language' content='pt' />
+      <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+    </Head>
+  </>
 )
 
 export default CustomHead
