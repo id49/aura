843acfcf5f30984310f49b117a45644a5f092c20
Renato Siqueira <renatoelysiqueira@gmail.com>
2021-09-28 18:46:39 -0300

    confirm email component

---
+++ components/ConfirmEmail/index.tsx
@@ -0,0 +1,10 @@
+const ConfirmEmail = () => (
+  <div className='py-20 lg:py-44'>
+    <p className='text-lg font-extrabold text-gray-500'>Confirme sua conta</p>
+    <p className='text-md font-light text-gray-500'>
+      Verifique seu email para confirmar sua conta.
+    </p>
+  </div>
+)
+
+export default ConfirmEmail
