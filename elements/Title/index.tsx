62895411e4cbfcdb954c0c56e33a24892b30db4c
Renato Siqueira <renatoelysiqueira@gmail.com>
2021-10-07 16:11:35 -0300

    cleanup

--- elements/Title/index.tsx
+++ elements/Title/index.tsx
@@ -7,9 +7,7 @@ interface Props {
 
 const Title = ({ text, subText }: Props) => (
   <div className='mt-3'>
-    <h1 className='text-2xl font-semibold text-gray-700 dark:text-gray-200'>
-      {text}
-    </h1>
+    <h1 className='text-2xl font-semibold text-gray-600'>{text}</h1>
     {subText && <p className='text-gray-500'>{subText}</p>}
   </div>
 )
