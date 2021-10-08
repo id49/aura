16b934442c1eb4365acd37967dd4c32e7f6fc66e
Renato Siqueira <renatoelysiqueira@gmail.com>
2021-10-07 22:42:58 -0300

    returned to purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,10 @@ const windmill = require('@learn49/aura-ui/config')
 
 module.exports = windmill({
   purge: [
-    // './pages/**/*.{js,ts,jsx,tsx}',
-    // './components/**/*.{js,ts,jsx,tsx}',
-    // './elements/**/*.{js,ts,jsx,tsx}',
-    // './layouts/**/*.{js,ts,jsx,tsx}'
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './elements/**/*.{js,ts,jsx,tsx}',
+    './layouts/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
     extend: {
