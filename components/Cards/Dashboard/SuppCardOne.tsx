193a8c05e22cfecb4ab5ceeb86e0b12390ee0efa
Renato Siqueira <renatoelysiqueira@gmail.com>
2021-10-26 19:08:20 -0300

    hover

--- components/Cards/Dashboard/SuppCardOne.tsx
+++ components/Cards/Dashboard/SuppCardOne.tsx
@@ -48,7 +48,7 @@ const SuppCardOne = () => (
       {supportChannels.map(({ url, text, image }) => (
         <div key={text} className='text-gay-600 font-medium mt-2'>
           <a href={url} target='_blank'>
-            <div className='flex gap-2 items-center bg-gray-200 hover:bg-gray-300 py-2 px-2 rounded-lg'>
+            <div className='flex gap-2 items-center bg-gray-200 hover:bg-gray-100 py-2 px-2 rounded-lg'>
               <Image src={image} width={32} height={32} alt={text} />
               <p className='text-gray-600 text-sm'>{text}</p>
             </div>
