6e1371e5ab59509a26ae99bcf08dff3e2ce67fca
Renato Siqueira <renatoelysiqueira@gmail.com>
2021-10-26 18:29:23 -0300

    added key

--- components/Cards/Dashboard/SuppCardOne.tsx
+++ components/Cards/Dashboard/SuppCardOne.tsx
@@ -45,7 +45,7 @@ const SuppCardOne = () => (
         Canais de Comunicação
       </p>
       {supportChannels.map(({ url, text, image }) => (
-        <div className='text-gay-600 font-medium mt-2'>
+        <div key={text} className='text-gay-600 font-medium mt-2'>
           <a href={url} target='_blank'>
             <div className='flex gap-2 items-center bg-gray-200 hover:bg-gray-300 py-2 px-2 rounded-lg'>
               <Image src={image} width={32} height={32} alt={text} />
