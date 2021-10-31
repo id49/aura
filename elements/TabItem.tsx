36e81a40a921f4ae88bdcd92dcafea7bec644f84
Renato Siqueira <renatoelysiqueira@gmail.com>
2021-10-31 19:25:24 -0300

    replacement colors WM

--- elements/TabItem.tsx
+++ elements/TabItem.tsx
@@ -12,7 +12,7 @@ const LessonTabItem = ({ id, title, show, action }: IProps) => {
   const defaultStyle = 'border-b-2 cursor-pointer px-1 pr-4 bg-gray-100'
   const styles = classNames(
     defaultStyle,
-    show === id && 'border-purple-600 text-purple-700'
+    show === id && 'border-gray-600 text-gray-700'
   )
 
   return (
