3030af24ff20371cb655ec482f0820ca359448b0
Renato Siqueira <renatoelysiqueira@gmail.com>
2021-10-31 19:06:29 -0300

    testing without purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,8 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 const windmill = require('@learn49/aura-ui/config')
 
 module.exports = windmill({
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  // purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: [],
   theme: {
     extend: {
       fontFamily: {
