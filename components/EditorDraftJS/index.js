84d3c15c30da5efa80506cc1f98143ce1cadc2c6
Renato Siqueira <renatoelysiqueira@gmail.com>
2021-11-02 20:42:21 -0300

    added break checking

--- components/EditorDraftJS/index.js
+++ components/EditorDraftJS/index.js
@@ -1,10 +1,10 @@
+import { useCurrentWidth } from 'react-breakpoints-hook'
 import { EditorState, convertFromRaw } from 'draft-js'
 import Editor from 'draft-js-plugins-editor'
 import createEmbedPlugin from 'draft-js-embed-plugin'
 import createImagePlugin from 'draft-js-image-plugin'
 import createVideoPlugin from 'draft-js-video-plugin'
 import createLinkifyPlugin from '@draft-js-plugins/linkify'
-import '@draft-js-plugins/linkify/lib/plugin.css'
 
 const embedPlugin = createEmbedPlugin()
 const imagePlugin = createImagePlugin()
@@ -14,12 +14,19 @@ const linkifyPlugin = createLinkifyPlugin()
 const plugins = [imagePlugin, embedPlugin, videoPlugin, linkifyPlugin]
 
 const EditorDraftJS = ({ parsedBody }) => {
+  let width = useCurrentWidth()
+
   const editorState = parsedBody
     ? EditorState.createWithContent(convertFromRaw(parsedBody))
     : EditorState.createEmpty()
 
   return (
-    <div className='lg:container pt-2 text-gray-800 bg-gray-100'>
+    <div
+      className='lg:container pt-2 text-gray-800 bg-gray-100'
+      style={{
+        minHeight: width > 1024 && 540
+      }}
+    >
       <Editor editorState={editorState} plugins={plugins} readOnly />
     </div>
   )
