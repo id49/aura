22316e7b530adf1ad89d3e9ff4c528ee7ed11a8d
Renato Siqueira <renatoelysiqueira@gmail.com>
2021-12-24 10:18:49 -0300

    included Saas live

--- pages/app/index.tsx
+++ pages/app/index.tsx
@@ -1,6 +1,12 @@
 import React, { useContext } from 'react'
+import Image from 'next/image'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Autoplay, Pagination, Navigation } from 'swiper'
 import { useQuery } from 'urql'
 import Link from 'next/link'
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
 
 import { AccountContext } from '@/context/AccountContext'
 import Title from '@/elements/Title'
@@ -55,78 +61,129 @@ const Dashboard = () => {
   })
   const { data, fetching } = result
 
+  const sliderData = [
+    {
+      img: '/courses/backgrounds/AplicandoTestescomNestJs.png',
+      title: 'Aplicando Testes no NestJS',
+      titleColor: '#fff',
+      description: 'Lorem ipsum here',
+      descriptionColor: '#fff'
+    },
+    {
+      img: '/courses/SaasEMultiTenancy.png',
+      title: 'SaaS e Multi-Tenancy',
+      titleColor: '#fff',
+      description: 'Lorem ipsum here',
+      descriptionColor: '#fff'
+    }
+  ]
+
   return (
-    <div className='container px-2 lg:px-1 py-6 mx-auto'>
-      <Head title='Dashboard' />
-      {!fetching &&
-        Object.keys(data).length > 0 &&
-        Object.keys(fullCourses).includes(
-          data?.getLastCourseAccess?.courseId
-        ) && (
-          <CardLastCourseAccess
-            courseName={fullCourses[data.getLastCourseAccess?.courseId].title}
-            {...data?.getLastCourseAccess}
-          />
-        )}
-      <Title text='Destaques' />
-      <CardTop3
-        courseOne={Object.values(fullCourses)[0]}
-        courseTwo={Object.values(fullCourses)[1]}
-        courseThree={Object.values(fullCourses)[2]}
-      />
-      <Title
-        text='Especialize-se!'
-        subText='Cursos específicos e direto ao ponto'
-      />
-      <div className='py-4 grid lg:grid-cols-2 gap-4'>
-        {Object.values(fullCourses)
-          .filter((key, id) => id > 2 && id < 10 && key?.id)
-          .map((each, i) => (
-            <CardCourses key={i} {...each} />
-          ))}
-      </div>
-
-      <div className='py-4 gap-4'>
-        <Title text='Projetos' subText='Aumente sua experiência prática!' />
-        {[13, 14, 15].map((courseId) => (
-          <Link
-            key={courseId}
-            href={`/app/courses/${Object.values(fullCourses)[courseId].id
-              }/version/${Object.values(fullCourses)[courseId].version}`}
-          >
-            <a>
-              <div className='container px-6 my-2 py-8 mx-auto bg-gray-100 hover:bg-gray-300 rounded-lg'>
-                <div className='items-center lg:flex'>
-                  <div className='lg:w-1/2 xl:w-2/3'>
-                    <h2 className='text-3xl font-bold text-gray-600'>
-                      {Object.values(fullCourses)[courseId].title}
-                    </h2>
-
-                    <p className='mt-4 text-gray-500'>
-                      {Object.values(fullCourses)[courseId].description}
-                    </p>
+    <>
+      {/* <div>
+        <Swiper
+          modules={[Autoplay, Pagination, Navigation]}
+          loop={true}
+          pagination={true}
+        >
+          {sliderData.map((e) => (
+            <SwiperSlide>
+              <div className='w-full h-screen max-h-xl relative'>
+                <Image className='z-10' src={e.img} layout='fill' />
+                <div className='mx-20 absolute bottom-36 z-20'>
+                  <div
+                    className='font-semibold text-6xl'
+                    style={{
+                      color: e.titleColor
+                    }}
+                  >
+                    {e.title}
                   </div>
+                  <div
+                    className='text-2xl py-2 font-serif italic'
+                    style={{
+                      color: e.titleColor
+                    }}
+                  >
+                    {e.description}
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div> */}
+      <div className='container px-2 lg:px-1 py-6 mx-auto'>
+        <Head title='Dashboard' />
+        {!fetching &&
+          Object.keys(data).length > 0 &&
+          Object.keys(fullCourses).includes(
+            data?.getLastCourseAccess?.courseId
+          ) && (
+            <CardLastCourseAccess
+              courseName={fullCourses[data.getLastCourseAccess?.courseId].title}
+              {...data?.getLastCourseAccess}
+            />
+          )}
+        <Title text='Destaques' />
+        <CardTop3
+          courseOne={Object.values(fullCourses)[0]}
+          courseTwo={Object.values(fullCourses)[1]}
+          courseThree={Object.values(fullCourses)[2]}
+        />
+        <Title
+          text='Especialize-se!'
+          subText='Cursos específicos e direto ao ponto'
+        />
+        <div className='py-4 grid lg:grid-cols-2 gap-4'>
+          {Object.values(fullCourses)
+            .filter((key, id) => id > 2 && id < 10 && key?.id)
+            .map((each, i) => (
+              <CardCourses key={i} {...each} />
+            ))}
+        </div>
+        <div className='py-4 gap-4'>
+          <Title text='Projetos' subText='Aumente sua experiência prática!' />
+          {[13, 14, 15, 16].map((courseId) => (
+            <Link
+              key={courseId}
+              href={`/app/courses/${Object.values(fullCourses)[courseId].id
+                }/version/${Object.values(fullCourses)[courseId].version}`}
+            >
+              <a>
+                <div className='container px-6 my-2 py-8 mx-auto bg-gray-100 hover:bg-gray-300 rounded-lg'>
+                  <div className='items-center lg:flex'>
+                    <div className='lg:w-1/2 xl:w-2/3'>
+                      <h2 className='text-3xl font-bold text-gray-600'>
+                        {Object.values(fullCourses)[courseId].title}
+                      </h2>
 
-                  <div className='mt-8 lg:mt-0 lg:w-1/2 xl:w-1/3'>
-                    <div className='flex items-center justify-center lg:justify-end'>
-                      <div className='max-w-lg'>
-                        <img
-                          className='object-cover object-center w-full h-64 rounded-md shadow'
-                          src={Object.values(fullCourses)[courseId].image}
-                          alt='Projeto'
-                        />
+                      <p className='mt-4 text-gray-500'>
+                        {Object.values(fullCourses)[courseId].description}
+                      </p>
+                    </div>
+
+                    <div className='mt-8 lg:mt-0 lg:w-1/2 xl:w-1/3'>
+                      <div className='flex items-center justify-center lg:justify-end'>
+                        <div className='max-w-lg'>
+                          <img
+                            className='object-cover object-center w-full h-64 rounded-md shadow'
+                            src={Object.values(fullCourses)[courseId].image}
+                            alt='Projeto'
+                          />
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            </a>
-          </Link>
-        ))}
+              </a>
+            </Link>
+          ))}
+        </div>
+        <Support />
+        <Copyright />
       </div>
-      <Support />
-      <Copyright />
-    </div>
+    </>
   )
 }
 
